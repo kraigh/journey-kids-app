8446f77e5dfdf4f93d966eea2f3a3d01f4717a99
Kraig Hufstedler <kraigory@gmail.com>
2014-08-12 01:30:01 -0500

    cleanup for testing presentation

--- window.js
+++ window.js
@@ -1,13 +1,13 @@
 window.onload = function() {
-  document.querySelector('#greeting').innerText =
-    'Hello, World! It is ' + new Date();
+  // document.querySelector('#greeting').innerText =
+  //   'Hello, World! It is ' + new Date();
   //chrome.app.window.current().fullscreen();
   var injectIt = function() {
     console.log('foobar?');
     webview.executeScript({ file: "jsPrintSetup.js" });
   }
-  var webview = document.getElementById("test");
-  webview.addEventListener("loadstop", injectIt);
+  //var webview = document.getElementById("test");
+  //webview.addEventListener("loadstop", injectIt);
   // webview.onload();
   //webview.executeScript({ code: "document.body.style.backgroundColor = 'red'" });
   // webview.addEventListener('load', function(e) {
