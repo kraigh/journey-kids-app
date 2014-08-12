cc30520ec480deb941c1cd200a97a84451019f91
Kraig Hufstedler <kraigory@gmail.com>
2014-08-12 00:50:28 -0500

    trying to execute script inside webview

--- window.js
+++ window.js
@@ -3,6 +3,20 @@ window.onload = function() {
     'Hello, World! It is ' + new Date();
   //chrome.app.window.current().fullscreen();
   chrome.app.window.current().contentWindow
+  var webview = document.getElementById("test");
+  webview.addEventListener("loadstop", injectIt);
+  // webview.onload();
+  //webview.executeScript({ code: "document.body.style.backgroundColor = 'red'" });
+  // webview.addEventListener('load', function(e) {
+  //   console.log("jsPrintSetup");
+  // });
   
+  var injectIt = function() {
+    console.log('foobar?');
+    webview.executeScript({ code: "console.log('foobar!');" });
+  }
 
 };
+
+
+
