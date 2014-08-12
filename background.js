d410a63b59e88477f6def54fc62db1beca3de549
Kraig Hufstedler <kraigory@gmail.com>
2014-08-11 22:54:02 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,16 @@
+/**
+ * Listens for the app launching, then creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  console.log('foobar3');
+  chrome.app.window.create(
+    "window.html",
+    {
+      id: "mainWindow"
+    }
+  );
+  console.log('foobar');
+});
