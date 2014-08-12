8bebe8118c79101a017c7ede4efcdfc7ab71eea5
Kraig Hufstedler <kraigory@gmail.com>
2014-08-12 00:59:15 -0500

    testing adding object from parent

---
+++ jsPrintSetup.js
@@ -0,0 +1,5 @@
+console.log('foobar!');
+var jsPrintSetup = new Object();
+jsPrintSetup.test = function(){
+    console.log('tested!');
+}
--- window.js
+++ window.js
@@ -2,7 +2,10 @@ window.onload = function() {
   document.querySelector('#greeting').innerText =
     'Hello, World! It is ' + new Date();
   //chrome.app.window.current().fullscreen();
-  chrome.app.window.current().contentWindow
+  var injectIt = function() {
+    console.log('foobar?');
+    webview.executeScript({ file: "jsPrintSetup.js" });
+  }
   var webview = document.getElementById("test");
   webview.addEventListener("loadstop", injectIt);
   // webview.onload();
@@ -11,11 +14,6 @@ window.onload = function() {
   //   console.log("jsPrintSetup");
   // });
   
-  var injectIt = function() {
-    console.log('foobar?');
-    webview.executeScript({ code: "console.log('foobar!');" });
-  }
-
 };
 
 
